3922d0ac021e5ae1700ddbb34b6c309fc5f0b971
空白 <92154428+SummCoder@users.noreply.github.com>
2023-10-06 21:40:48 +0800

    Add files via upload

---
+++ homework4/homework/src/main/java/contactsystem/ContactController.java
@@ -0,0 +1,49 @@
+package contactsystem;
+
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.stereotype.Controller;
+import org.springframework.validation.Errors;
+import org.springframework.web.bind.annotation.*;
+
+import javax.validation.Valid;
+
+@Slf4j
+@Controller
+@RequestMapping("/")
+@SessionAttributes("tacoOrder")
+
+public class ContactController {
+
+
+    @ModelAttribute(name = "contact")
+    public Contact contact() {
+        return new Contact();
+    }
+
+    // 返回页面
+    @GetMapping
+    public String showContactForm(){
+        return "home";
+    }
+
+    @ModelAttribute(name = "contacts")
+    public ContactRepository contacts() {
+        return new ContactRepositoryImpl();
+    }
+
+    @PostMapping
+    public String processContact(
+            @Valid Contact contact, Errors errors,
+            @ModelAttribute ContactRepositoryImpl contacts) {
+
+        if (errors.hasErrors()) {
+            return "home";
+        }
+
+        contacts.save(contact);
+
+        return "redirect:/";
+    }
+
+
+}
